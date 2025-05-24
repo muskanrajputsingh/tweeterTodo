e3aac2eb9bbf49b13f9422e2a48223febe4755e3
Muskan Singh <muskansingh@Muskans-MacBook-Air.local>
2025-05-24 17:08:46 +0530

    feat: add ads

--- tweetprac/src/pages/home-tweet/dataa.js
+++ tweetprac/src/pages/home-tweet/dataa.js
@@ -7,8 +7,20 @@ export const ads = [
     },
     {
         "id": 2,
-        "image": "https://images.pexels.com/photos/853261/pexels-photo-853261.jpeg",
+        "image": "https://images.pexels.com/photos/1695052/pexels-photo-1695052.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         "description": "Fresh coffee beans delivered fast right to your doorstep now!",
+        "link": "https://www.example.com/coffee-subscription"
+      },
+      {
+        "id": 3,
+        "image": "https://images.pexels.com/photos/267320/pexels-photo-267320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        "description": "Latest new stylish Footwear delivered fast right to your doorstep now!",
+        "link": "https://www.example.com/coffee-subscription"
+      },
+      {
+        "id": 4,
+        "image": "https://images.pexels.com/photos/25533521/pexels-photo-25533521/free-photo-of-selective-focus-of-lipsticks.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        "description": "Lipstick sale Live now Grab Your Lipsticks in Affordable pices!",
         "link": "https://www.example.com/coffee-subscription"
       },
   ];
@@ -16,13 +28,13 @@ export const ads = [
   export const add2 = [
     {
         "id": 3,
-        "image": "https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg",
+        "image": "https://images.pexels.com/photos/1092671/pexels-photo-1092671.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         "description": "Boost your productivity with our latest mobile productivity app!",
         "link": "https://www.example.com/apps/productivity"
       },
       {
         "id": 4,
-        "image": "https://images.pexels.com/photos/3952237/pexels-photo-3952237.jpeg",
+        "image": "https://images.pexels.com/photos/9561301/pexels-photo-9561301.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load",
         "description": "Luxury watches at affordable prices – limited time collection offer!",
         "link": "https://www.example.com/fashion/watches"
       }
